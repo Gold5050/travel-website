03e6bf9d49b3a7dee5a9f8f009e2f3db6efdff51
unknown <henryifeanyi9933@gmail.comgit config --golbal user.name ifeanyi>
2023-11-30 19:53:21 +0100

    my travel website

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+const preloader = document.querySelector("[data-preloader]");
+
+window.addEventListener("load", () => {
+    preloader.classList.add("remove");
+}) 
+    
+const addEventOnElements = function (elements, eventType, callback){
+    for (let i = 0, len = elements.length; i < len; i++){
+        elements[i].addEventListener(eventType, callback);
+    }
+}
+
+// this is navbar toggle for mobile screen
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNav = function (){
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+    document.body.classList.toggle("nav-active");
+}
+
+addEventOnElements(navTogglers, "click", toggleNav);
+
+// this is for the headers
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function (){
+    header.classList[window.scrollY > 100 ? "add" : "remove"]("active");
+})
